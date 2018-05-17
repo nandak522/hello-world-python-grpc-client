364b375886be9c9bfa07b464cd5880428e9db51e
Nanda Kishore <madhav.bnk@gmail.com>
2018-05-17 16:29:34 +0530

    Make the host, port configurable

--- client.py
+++ client.py
@@ -3,8 +3,11 @@ from hello_world_python_grpc.service import hello_world_pb2_grpc
 from hello_world_python_grpc.service import hello_world_pb2
 
 
+SERVER_HOST = 'localhost'
+SERVER_PORT = '32260'
+
 def main():
-    channel = grpc.insecure_channel("localhost:8888")
+    channel = grpc.insecure_channel("{}:{}".format(SERVER_HOST, SERVER_PORT))
     stub = hello_world_pb2_grpc.HelloWorldGreeterStub(channel)
     request = hello_world_pb2.HelloWorldRequest(greeting="Hello World!")
     response = stub.SayHello(request)
---
+++ curl_rpc_v1_v2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for i in {1..150}
+do
+   /Users/nanda/sources/tryouts/hello-world-python-grpc-client/bin/python client.py
+   # echo "Exit code: $?"
+done
